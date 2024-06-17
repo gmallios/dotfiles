9e9fa59ec6b1a2b11e20c4737dc4396d1e9bd1e6
Grigoris Mallios <gregmallios@gmail.com>
2024-06-18 01:20:49 +0300

    initial nvim configuration using astronvim

---
+++ nvim/.config/nvim/lua/plugins/community.lua
@@ -0,0 +1,8 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+}
