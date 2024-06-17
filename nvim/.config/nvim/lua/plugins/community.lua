af823415617ed89adc15aa23124d6f701a2d822a
Grigoris Mallios <gregmallios@gmail.com>
2024-06-18 01:33:54 +0300

    use correct lua for file for community

--- nvim/.config/nvim/lua/plugins/community.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.toml" },
-  { import = "astrocommunity.pack.yaml" },
-}
